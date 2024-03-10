27b36301ac3fce97abe60e166d287ce7273ff174
vitoria31032003 <162328614+vitoria31032003@users.noreply.github.com>
2024-03-10 14:58:27 -0300

    Update Scb.c

--- Scb.c
+++ Scb.c
@@ -2,7 +2,7 @@
 ///   Universidade Federal do Rio Grande do Norte     ///
 ///   Centro de Ensino Superior do Seridó             ///
 ///   Departamento de Computação e Tecnologia         ///
-///   Disciplina: Programação						              ///
+///   Disciplina: Programação                         ///
 ///   Professor: Flavius Gorgônio					            ///
 ///	  Projeto: Sistema de Controle de Biblioteca      ///
 ///   Aluna: Vitória Geovanna de Assis Pereira        ///
