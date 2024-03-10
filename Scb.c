1c8fb41bd7191fccb6e8a9581feee35a35ce7279
vitoria31032003 <162328614+vitoria31032003@users.noreply.github.com>
2024-03-10 15:59:48 -0300

    Update Scb.c

--- Scb.c
+++ Scb.c
@@ -71,30 +71,30 @@ void tela_menu_principal(void){
 
 
 void tela_menu_sobre(void){
-	system("clear||cls");
+  system("clear||cls");
   printf("\n");
-	printf("**************************************************************\n");
+  printf("**************************************************************\n");
   printf("***   Universidade Federal do Rio Grande do Norte          ***\n");
-	printf("***   Centro de Ensino Superior do Serido                  ***\n");
-	printf("***   Departamento de Computacao e Tecnologia              ***\n");
-	printf("***   Disciplina: Programacao                              ***\n");
-	printf("***   Professor: Flavius Gorgonio                          ***\n");
-	printf("***   Projeto: Sistema de Controle de Biblioteca           ***\n");
-	printf("***   Aluna: Vitoria Geovanna de Assis Pereira             ***\n");
-	printf("***                                                   	   ***\n");    
-	printf("**************************************************************\n");
-	printf("***                                                        ***\n");
-	printf("***   = = = Sistema Controle de Biblioteca = = =           ***\n");
-	printf("***                                                        ***\n");
-	printf("***   Programa utilizado na disciplina DC1106-Programacao, ***\n");
-	printf("***   para fins avaliativo e aprendizado. O programa tem   ***\n");
-	printf("***   os principais modulos e funcionalidades que o tema   ***\n");
-	printf("***   deve ter.                                            ***\n");
-	printf("***                                                        ***\n");
-	printf("///                                                        ///\n");
-	printf("//////////////////////////////////////////////////////////////\n");
-	printf("\n");
-	printf("\t Pressione <ENTER> para continuar...\n");
-	getchar();
+  printf("***   Centro de Ensino Superior do Serido                  ***\n");
+  printf("***   Departamento de Computacao e Tecnologia              ***\n");
+  printf("***   Disciplina: Programacao                              ***\n");
+  printf("***   Professor: Flavius Gorgonio                          ***\n");
+  printf("***   Projeto: Sistema de Controle de Biblioteca           ***\n");
+  printf("***   Aluna: Vitoria Geovanna de Assis Pereira             ***\n");
+  printf("***                                                        ***\n");    
+  printf("**************************************************************\n");
+  printf("***                                                        ***\n");
+  printf("***   = = = Sistema Controle de Biblioteca = = =           ***\n");
+  printf("***                                                        ***\n");
+  printf("***   Programa utilizado na disciplina DC1106-Programacao, ***\n");
+  printf("***   para fins avaliativo e aprendizado. O programa tem   ***\n");
+  printf("***   os principais modulos e funcionalidades que o tema   ***\n");
+  printf("***   deve ter.                                            ***\n");
+  printf("***                                                        ***\n");
+  printf("///                                                        ///\n");
+  printf("//////////////////////////////////////////////////////////////\n");
+  printf("\n");
+  printf("\t Pressione <ENTER> para continuar...\n");
+  getchar();
 
 }
