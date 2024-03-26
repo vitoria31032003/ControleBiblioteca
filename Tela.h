b711d969872fdf983514a0ade56f0630050eda00
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-03-26 14:59:17 -0300

    Add files via upload

---
+++ Tela.h
@@ -0,0 +1,29 @@
+#ifndef TELAS_H
+#define TELAS_H
+
+
+void tela_menu_principal(void);
+void tela_menu_sobre(void);
+void tela_desenvolvedora(void);
+void tela_menu_login(void);
+void tela_menu_funcionarios(void);
+void tela_menu_clientes_usuarios(void);
+void tela_menu_acervo(void);
+void entrar_como_administrador(void);
+void entrar_como_funcionario(void);
+void entrar_como_cliente_usuario(void);
+void cadastrar_cliente_usuario(void);
+void solicitar_cadastro(void);
+void solicitar_emprestimo(void);
+void solicitar_renovacao(void);
+void alterar_dados_pessoais(void);
+void cadastrar_livros(void);
+void liberar_emprestimo(void);
+void liberar_renovacao(void);
+void registar_devolucao(void);
+void remover_livros(void);
+void atualizar_livros(void);
+void quantidade_livros(void);
+
+
+#endif
