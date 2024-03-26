16c340e8bd283ec39ae675eb5ef5bb6422d8a5d4
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-03-26 19:11:27 -0300

    Update Tela.h

--- Tela.h
+++ Tela.h
@@ -1,3 +1,13 @@
+/////////////////////////////////////////////////////////
+///   Universidade Federal do Rio Grande do Norte     ///
+///   Centro de Ensino Superior do Seridó             ///
+///   Departamento de Computação e Tecnologia         ///
+///   Disciplina: Programação                         ///
+///   Professor: Flavius Gorgônio                     ///
+///   Projeto: Sistema de Controle de Biblioteca      ///
+///   Aluna: Vitória Geovanna de Assis Pereira        ///
+/////////////////////////////////////////////////////////
+
 #ifndef TELAS_H
 #define TELAS_H
 
