4dc218c5749b9234a01676ea74901bdf9cb43272
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-05 20:33:09 -0300

    Add files via upload

---
+++ TelaMenuPrincipal.c
@@ -0,0 +1,28 @@
+
+void tela_menu_principal(void)
+{
+	int op = 100;
+	printf("***************************************************************\n");
+	printf("***   Universidade Federal do Rio Grande do Norte           ***\n");
+	printf("***   Centro de Ensino Superior do Serido                   ***\n");
+	printf("***   Departamento de Computacao e Tecnologia               ***\n");
+	printf("***   Disciplina: Programacao                               ***\n");
+	printf("***   Professor: Flavius Gorgonio                           ***\n");
+	printf("***   Projeto: Sistema de Controle de Biblioteca            ***\n");
+	printf("***   Aluna: Vitoria Geovanna de Assis Pereira              ***\n");
+	printf("***                                                         ***\n");
+	printf("***************************************************************\n");
+	printf("***                                                         ***\n");
+	printf("***  = = = Sistema Controle de Biblioteca = = =             ***\n");
+	printf("***                                                         ***\n");
+	printf("***  1. Modulo Login                                        ***\n");
+	printf("***  2. Modulo Funcionarios                                 ***\n");
+	printf("***  3. Modulo Clientes/Usuarios                            ***\n");
+	printf("***  4. Modulo Acervo                                       ***\n");
+	printf("***  0. Sair                                                ***\n");
+	printf("***                                                         ***\n");
+	printf("***  Informe a opcao desejada:                              ***\n");
+	printf("***                                                         ***\n");
+	scanf("%d", &op);
+	getchar();
+}
