86fd49aee1c67b167fb7ab61cc6cef4aa5c2264b
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-06 11:02:15 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Seridó                 ///
+/// Departamento de Computação e Tecnologia             ///
+/// Disciplina: Programação                             ///
+/// Professor: Flavius Gorgônio                         ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira            ///
+///////////////////////////////////////////////////////////
+
+
+int main(void) {
+  
+  menu_principal();
+  
+  return 0;
+}
