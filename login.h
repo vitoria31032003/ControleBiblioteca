7ca2f7ce3742fe68cad97dc563bc2573163333d5
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-06 14:53:40 -0300

    login.h

---
+++ login.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Seridó                 ///
+/// Departamento de Computação e Tecnologia             ///
+/// Disciplina: Programação                             ///
+/// Professor: Flavius Gorgônio                         ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira            ///
+///////////////////////////////////////////////////////////
+
+
+#ifndef LOGIN_H
+#define LOGIN_H
+
+
+void tela_menu_login(void);
+void entrar_como_administrador(void);
+void entrar_como_funcionario(void);
+void entrar_como_cliente_usuario(void);
+
+#endif
