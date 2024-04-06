3bed5a1e0c80015403e170f3c60d6e62633e1510
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-06 11:34:59 -0300

    Update sobre.c

--- sobre.c
+++ sobre.c
@@ -12,3 +12,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sobre.h"
+
+
+
+void tela_menu_sobre(void){
+	system("clear||cls");
+  printf("\n");
+	printf("**************************************************************\n");
+  printf("***   Universidade Federal do Rio Grande do Norte          ***\n");
+	printf("***   Centro de Ensino Superior do Serido                  ***\n");
+	printf("***   Departamento de Computacao e Tecnologia              ***\n");
+	printf("***   Disciplina: Programacao                              ***\n");
+	printf("***   Professor: Flavius Gorgonio                          ***\n");
+	printf("***   Projeto: Sistema de Controle de Biblioteca           ***\n");
+	printf("***   Aluna: Vitoria Geovanna de Assis Pereira             ***\n");
+	printf("***                                                   	   ***\n");    
+	printf("**************************************************************\n");
+	printf("***                                                        ***\n");
+	printf("***   = = = Sistema Controle de Biblioteca = = =           ***\n");
+	printf("***                                                        ***\n");
+	printf("***   Programa utilizado na disciplina DC1106-Programacao, ***\n");
+	printf("***   para fins avaliativo e aprendizado. O programa tem   ***\n");
+	printf("***   os principais modulos e funcionalidades que o tema   ***\n");
+	printf("***   deve ter.                                            ***\n");
+	printf("***                                                        ***\n");
+	printf("///                                                        ///\n");
+	printf("//////////////////////////////////////////////////////////////\n");
+	printf("\n");
+	printf("\t Pressione <ENTER> para continuar...\n");
+	getchar();
+
+}
