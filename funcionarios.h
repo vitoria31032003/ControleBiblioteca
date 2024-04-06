a7b050630fce7548651595938a0e02226416aa64
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-06 15:47:11 -0300

    Create funcionarios.h

---
+++ funcionarios.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Seridó                 ///
+/// Departamento de Computação e Tecnologia             ///
+/// Disciplina: Programação                             ///
+/// Professor: Flavius Gorgônio                         ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira            ///
+///////////////////////////////////////////////////////////
+
+
+#ifndef FUNCIONARIOS_H
+#define FUNCIONARIOS_H
+
+
+void cadastrar_cliente_usuario(void);
+void liberar_emprestimo(void);
+void liberar_renovacao(void);
+void registar_devolucao(void);
+    	
+#endif
