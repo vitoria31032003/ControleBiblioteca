3ac3246bf5fc8f32fdc670026a085216dc88ffc8
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-07 13:01:57 -0300

    acervo.c

--- acervo.c
+++ acervo.c
@@ -17,54 +17,54 @@
 
 void tela_menu_acervo(void)
 {
-	      int op = 100;
-	      system("clear||cls");
-	      printf("\n");
-	      printf("************************************************************\n");
-	      printf("***                                                      ***\n");
-	      printf("***  =================================================== ***\n");
-	      printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
-	      printf("***  = = = = Sistema de Controle de Biblioteca = = = = = ***\n");
-	      printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
-	      printf("***  =================================================== ***\n");
-	      printf("************************************************************\n");
-	      printf("***                                                      ***\n");
-	      printf("***  =================================================== ***\n");
-	      printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
-	      printf("***  = = = = = = = = Menu Acervo = = = = = = = = = = = = ***\n");
-	      printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
-	      printf("***  =================================================== ***\n");
-	      printf("***                                                      ***\n");
-	      printf("***  1. Cadastrar Livros                                 ***\n");
-	      printf("***  2. Remover Livros                                   ***\n");
-	      printf("***  3. Atualizar Livros                                 ***\n");
-      	      printf("***  4. Quantidade de Livros Disponiveis                 ***\n");
-	      printf("***  0. Voltar ao menu principal                         ***\n");
-	      printf("***                                                      ***\n");
-	      printf("***  Informe a opcao desejada:                           ***\n");
-	      printf("***                                                      ***\n");
-	      scanf("%d", &op);
-	      switch(op)
-	      {
-	      case 1:
-		     cadastrar_livros();
-		     break;
-	      case 2:
-		     remover_livros();
-		     break;
-	      case 3:
-		     atualizar_livros();
-		     break;
-	      case 4:
-		     quantidade_livros();
-		     break;
-              case 0:
-		     tela_menu_principal();
-		     break;
+	int op = 100;
+	system("clear||cls");
+	printf("\n");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = Sistema de Controle de Biblioteca = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+        printf("***  =================================================== ***\n");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = = = = = Menu Acervo = = = = = = = = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  =================================================== ***\n");
+        printf("***                                                      ***\n");
+	printf("***  1. Cadastrar Livros                                 ***\n");
+	printf("***  2. Remover Livros                                   ***\n");
+	printf("***  3. Atualizar Livros                                 ***\n");
+      	printf("***  4. Quantidade de Livros Disponiveis                 ***\n");
+	printf("***  0. Voltar ao menu principal                         ***\n");
+	printf("***                                                      ***\n");
+	printf("***  Informe a opcao desejada:                           ***\n");
+	printf("***                                                      ***\n");
+	scanf("%d", &op);
+	switch(op)
+	{
+	case 1:
+	       cadastrar_livros();
+	       break;
+	case 2:
+		remover_livros();
+		break;
+	case 3:
+		atualizar_livros();
+		break;
+	case 4:
+		quantidade_livros();
+		break;
+        case 0:
+		tela_menu_principal();
+		break;
       
-	      default:
-		      printf("Informe uma opcao valida");
-		      break;
+	default:
+		printf("Informe uma opcao valida");
+		break;
 	}
 }
 
