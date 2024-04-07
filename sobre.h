45c41ae2d3d2cc87e29dab51cb07efb6d8f9e1bc
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-07 13:18:57 -0300

    4sobre.h

---
+++ sobre.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Seridó                 ///
+/// Departamento de Computação e Tecnologia             ///
+/// Disciplina: Programação                             ///
+/// Professor: Flavius Gorgônio                         ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira            ///
+///////////////////////////////////////////////////////////
+
+
+#ifndef SOBRE_H
+#define SOBRE_H
+
+
+void tela_sobre(void);
+void tela_desenvolvedora(void);
+
+#endif
