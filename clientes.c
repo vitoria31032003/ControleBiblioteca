704e950e8777b06bf7490e6b515dc1c6a52425e3
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-07 00:02:27 -0300

    clientes.c

--- clientes.c
+++ clientes.c
@@ -30,43 +30,44 @@ void tela_menu_clientes_usuarios(void)
 	      printf("************************************************************\n");
 	      printf("***                                                      ***\n");
 	      printf("***  =================================================== ***\n");
-      	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+      	      printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
 	      printf("***  = = = = = = = =  Menu Clientes/Usuarios = = = = = = ***\n");
 	      printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
 	      printf("***  =================================================== ***\n");
 	      printf("***                                                      ***\n");
 	      printf("***  1. Solicitar Cadastro                               ***\n");
-      	printf("***  2. Solicitar Emprestimo                             ***\n");
+      	      printf("***  2. Solicitar Emprestimo                             ***\n");
 	      printf("***  3. Solicitar Renovacao                              ***\n");
-      	printf("***  4. Alterar Dados Pessoais                           ***\n");
-      	printf("***  0. Voltar ao menu principal                         ***\n");
+       	      printf("***  4. Alterar Dados Pessoais                           ***\n");
+      	      printf("***  0. Voltar ao menu principal                         ***\n");
 	      printf("***                                                      ***\n");
 	      printf("***  Informe a opcao desejada:                           ***\n");
 	      printf("***                                                      ***\n");
 	      scanf("%d", &op);
 	      switch(op)
-      	{
-      	case 1:
-		      solicitar_cadastro();
-	      	break;
-    	 case 2:
-	    	  solicitar_emprestimo();
-	    	  break;
-	     case 3:
-		      solicitar_renovacao();
-		      break;
-	     case 4:
-	        alterar_dados_pessoais();
-		      break;
-	    case 0:
-	  	   tela_menu_principal();
+      	      {
+      	      case 1:
+		     solicitar_cadastro();
+	      	     break;
+    	      case 2:
+	    	     solicitar_emprestimo();
+	    	     break;
+	      case 3:
+		     solicitar_renovacao();
+		     break;
+	      case 4:
+	             alterar_dados_pessoais();
+		     break;
+	      case 0:
+	  	     tela_menu_principal();
 		     break;
 
-	   default:
-		     printf("Informe uma opcao valida");
-		     break;
+	      default:
+		       printf("Informe uma opcao valida");
+		       break;
 	}
 }
+
 
 
 void solicitar_cadastro(void)
@@ -154,7 +155,6 @@ void solicitar_renovacao(void)
 	printf("************************************************************\n");
 
 }
-
 
 
 
