d4c3ac2aa5a12b161fc336f3cdd046b6ee440e7b
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-08 11:45:06 -0300

    utilidade.h

---
+++ utilidade.h
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Seridó                 ///
+/// Departamento de Computação e Tecnologia             ///
+/// Disciplina: Programação                             ///
+/// Professor: Flavius Gorgônio                         ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira            ///
+///////////////////////////////////////////////////////////
+
+
+#ifndef UTILDADE_H
+#define UTILDADE_H
+
+
+void limpa_tela(void);
+
+#endif
