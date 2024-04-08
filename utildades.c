8d7db0f1d7335676f0b98ad0d9140dcac843946c
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-08 11:53:28 -0300

    utildades.c

---
+++ utildades.c
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Seridó                 ///
+/// Departamento de Computação e Tecnologia             ///
+/// Disciplina: Programação                             ///
+/// Professor: Flavius Gorgônio                         ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira            ///
+///////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include "utildades.h"
+
+
+////////////////////
+/// Limpa a tela ///
+////////////////////
+void limpa_tela(void){
+	if (system("clear") || system("cls")){
+		/// Função limpa a tela
+	}
+}
