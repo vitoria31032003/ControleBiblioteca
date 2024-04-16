0030f12cc6b6a35650e4128ba8ae0da672161725
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-04-15 22:49:24 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Serid�                 ///
+/// Departamento de Computação e Tecnologia           ///
+/// Disciplina: Programação                           ///
+/// Professor: Flavius Gorgônio                        ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira           ///
+///////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "biblioteca.h"
+
+int main(void){
+	
+	tela_menu_principal();
+	
+	return 0;
+}
