115c0934719ac2dd83f2b8ba5f4381f9f69ffd0c
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-05-24 22:28:58 -0300

    Add files via upload

---
+++ sobre.c
@@ -0,0 +1,73 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Seridó                 ///
+/// Departamento de Computação e Tecnologia             ///
+/// Disciplina: Programação                             ///
+/// Professor: Flavius Gorgônio                         ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira            ///
+///////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "sobre.h"
+
+
+
+void tela_sobre(void)
+{
+        system("clear||cls");
+        printf("\n");
+        printf("**************************************************************\n");
+        printf("***   Universidade Federal do Rio Grande do Norte          ***\n");
+	printf("***   Centro de Ensino Superior do Serido                  ***\n");
+	printf("***   Departamento de Computacao e Tecnologia              ***\n");
+	printf("***   Disciplina: Programacao                              ***\n");
+	printf("***   Professor: Flavius Gorgonio                          ***\n");
+	printf("***   Projeto: Sistema de Controle de Biblioteca           ***\n");
+	printf("***   Aluna: Vitoria Geovanna de Assis Pereira             ***\n");
+	printf("***                                                   	   ***\n");    
+	printf("**************************************************************\n");
+	printf("***                                                        ***\n");
+	printf("***   = = = Sistema Controle de Biblioteca = = =           ***\n");
+	printf("***                                                        ***\n");
+	printf("***   Programa utilizado na disciplina DC1106-Programacao, ***\n");
+	printf("***   Este programa permite gerenciar o acervo de livros,  ***\n");
+	printf("***   Ele inclui os principais modulos e funcionalidades   ***\n");
+	printf("***   como cadastrar, atualizar, verificar quantidade de   ***\n");
+	printf("***   livros disponiveis e entre outras funcionalidades.   ***\n");
+	printf("***                                                        ***\n");
+        printf("**************************************************************\n");
+	printf("\n");
+	printf("\t Pressione <ENTER> para continuar...\n");
+        getchar();
+}
+
+
+
+void tela_desenvolvedora(void)
+{
+        system("clear||cls");
+        printf("\n");
+        printf("**************************************************************\n");
+        printf("***   Universidade Federal do Rio Grande do Norte          ***\n");
+        printf("***   Centro de Ensino Superior do Serido                  ***\n");
+        printf("***   Departamento de Computacao e Tecnologia              ***\n");
+        printf("***   Disciplina: Programacao                              ***\n");
+        printf("***   Professor: Flavius Gorgonio                          ***\n");
+        printf("***   Projeto: Sistema de Controle de Biblioteca           ***\n");
+        printf("***   Aluna: Vitoria Geovanna de Assis Pereira             ***\n");
+        printf("***                                                        ***\n");    
+        printf("**************************************************************\n");
+        printf("***                                                        ***\n");
+        printf("***   = = = Sistema Controle de Biblioteca = = =           ***\n");
+        printf("***                                                        ***\n");
+        printf("***   Este projeto foi desenvolvido por:                   ***\n");
+        printf("***   Vitoria Geovanna de Assis Pereira                    ***\n");
+        printf("***   Git: https://github.com/vitoria31032003              ***\n");
+        printf("***   Rede social: @vitoriageovanna18                      ***\n");
+        printf("\n");
+        printf("\t Pressione <ENTER> para continuar...\n");
+        getchar();
+}
