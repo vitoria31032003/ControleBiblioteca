dd9f5160b72b0d4763ad71ccaf90d23f74fc3549
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-05-24 22:23:16 -0300

    Add files via upload

---
+++ login.c
@@ -0,0 +1,145 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Seridó                 ///
+/// Departamento de Computação e Tecnologia             ///
+/// Disciplina: Programação                             ///
+/// Professor: Flavius Gorgônio                         ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira            ///
+///////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "login.h"
+
+
+
+void tela_menu_login(void)
+{
+	int op = 100;
+	system("clear||cls");
+	printf("\n");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = Sistema de Controle de Biblioteca = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  =================================================== ***\n");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = = = = =  Menu Login = = = = = = = = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***                                                      ***\n");
+	printf("***  1. Entrar no sistema como administrador             ***\n");
+	printf("***  2. Entrar no sistema como funcionario               ***\n");
+	printf("***  3. Entrar no sistema como cliente/usuario           ***\n");
+	printf("***  0. Voltar ao menu principal                         ***\n");
+	printf("***                                                      ***\n");
+	printf("***  Informe a opcao desejada:                           ***\n");
+	printf("***                                                      ***\n");
+	scanf("%d", &op);
+	switch(op)
+	{
+	case 1:
+		entrar_como_administrador();
+		break;
+	case 2:
+		entrar_como_funcionario();
+		break;
+	case 3:
+		entrar_como_cliente_usuario();
+		break;
+	
+	default:
+		printf("Informe uma opcao valida");
+		break;
+	}
+}
+
+
+
+void entrar_como_administrador(void)
+{
+	system("clear||cls");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = Sistema de Controle de Biblioteca = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***                                                      ***\n");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = Entrar como Administrador = = = = = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***                                                      ***\n");
+	printf("***  CPF do Administrador:                               ***\n")
+	printf("***  Senha:                                              ***\n");
+	printf("***                                                      ***\n");
+	printf("************************************************************\n");
+}
+
+
+
+void entrar_como_funcionario(void)
+{
+	system("clear||cls");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = Sistema de Controle de Biblioteca = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***                                                      ***\n");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = Entrar como Funcionario = = = = = = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***                                                      ***\n");
+	printf("***  CPF do Funcionario:                                 ***\n");
+	printf("***  Senha:                                              ***\n");
+	printf("***                                                      ***\n");
+	printf("************************************************************\n");
+}
+
+
+
+void entrar_como_cliente_usuario(void)
+{
+	system("clear||cls");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = Sistema de Controle de Biblioteca = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***                                                      ***\n");
+	printf("************************************************************\n");
+	printf("***                                                      ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  = = = = Entrar como Clientes/Usuario  = = = = = = = ***\n");
+	printf("***  = = = = = = = = = = = = = = = = = = = = = = = = = = ***\n");
+	printf("***  =================================================== ***\n");
+	printf("***                                                      ***\n");
+	printf("***  CPF do Cliente:                                     ***\n");
+    printf("***  Senha:                                              ***\n");
+	printf("***                                                      ***\n");
+	printf("************************************************************\n");
+}
