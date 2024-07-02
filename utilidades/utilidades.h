0d7694a4576858218f54453de1e3b5e7342ae22b
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-07-02 15:03:41 -0300

    Add files via upload
    Organizando as funções utilidades

---
+++ utilidades/utilidades.h
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////
+/// Universidade Federal do Rio Grande do Norte         ///
+/// Centro de Ensino Superior do Seridó                 ///
+/// Departamento de Computação e Tecnologia             ///
+/// Disciplina: Programação                             ///
+/// Professor: Flavius Gorgônio                         ///
+/// Projeto: Sistema de Controle de Biblioteca          ///
+/// Aluna: Vitória Geovanna de Assis Pereira            ///
+///////////////////////////////////////////////////////////
+#include <stdbool.h>
+
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+
+bool val_senha(char *senha);
+void recebe_senha(char *senha);
+
+int val_nome(char *nome);
+
+bool val_CPF(char *cpf);
+int recebe_cpf(char *cpf);
+
+
+int val_data(int dia, int mes, int ano);
+int recebe_data(char *data);
+
+bool val_fone(char *fone);
+int recebe_fone(char *fone);
+
+void limparBufferEntrada(void);
+
+
+#endif
