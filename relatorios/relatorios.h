1bbca0eb1f198e35ef86789d0ff56fe129dda6e9
Vitória Geovanna <162328614+vitoria31032003@users.noreply.github.com>
2024-07-30 15:57:05 -0300

    Add files via upload

---
+++ relatorios/relatorios.h
@@ -0,0 +1,20 @@
+#ifndef RLATORIOS_H
+#define RLATORIOS_H
+
+void escolha_menu_relatorios(char op);
+void tela_menu_relatorio(void);
+
+void escolha_usuario(char op);
+void relatorioUsuario(void);
+void listar_usuarios(void);
+
+
+void escolha_funcionario(char op);
+void relatorioFuncionario(void);
+void listar_funcionario(void);
+
+void escolha_acervo(char op);
+void relatorioAcervo(void);
+void listar_acervo(void);
+
+#endif
